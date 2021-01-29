1adb1890f7b87db5f2e5edcdedf4d63be64f3aad
Karan Singh Negi <46072159+karansinghnegi@users.noreply.github.com>
2021-01-30 05:14:55 +0530

    Fixed bug

--- 7.Reach Router/src/Pet.js
+++ 7.Reach Router/src/Pet.js
@@ -4,7 +4,7 @@ export default function Pet({ name, animal, breed, media, location, id }) {
 
     let hero = 'http://placecorgi.com/300/300';
 
-    if(media.lenght) {
+    if(media.length) {
         hero = media[0].small;
     }
 
